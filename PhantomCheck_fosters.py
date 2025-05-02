434df83679447c53cf1b91bf07e31efa66583c6d
Xan McPherson <123395570+xannnimal@users.noreply.github.com>
2025-05-02 12:11:01 -0700

    Add files via upload

--- PhantomCheck_fosters.py
+++ PhantomCheck_fosters.py
@@ -16,48 +16,35 @@ import matplotlib.pyplot as plt
 import mne
 import mne.utils
 import matplotlib 
+import mat73
 import scipy.io
 
 # Run to load Phantom Data to save for MATLAB processing #
-raw_dir = '~/RESEARCH/Data/ILABS/Phantom/240708'
+raw_dir = '~/RESEARCH/Data/ILABS/Phantom/231207'
 avg_file = ''
-raw_file = 'phantom_32_200nam_20240708_raw.fif'
-correct_raw = mne.io.read_raw_fif(os.path.join(raw_dir, raw_file),
+#raw_file = 'phantom_32_200nam_20240708_raw.fif'
+raw_file = 'phantom_1000nam_default_IASoff_raw.fif'
+raw = mne.io.read_raw_fif(os.path.join(raw_dir, raw_file),
                           preload=False, allow_maxshield='yes')
-## use the "raw_file" full version in MATLAB, then don't crop data here
-raw_short = correct_raw.copy().crop(tmax=60).load_data() 
-correct_info = raw_short.info
-##########
-#use matlab matrix to create new raw with our processed data
-datarec_sss = scipy.io.loadmat('datarec_sss.mat')
-data_sss = datarec_sss["data_rec_vsh"]
-raw = mne.io.RawArray(data_sss, raw_short.info)
+## uncomment to save covariance for Foster's in matlab
+# cov_raw = mne.compute_raw_covariance(raw)
+# scipy.io.savemat("1000nam_default_IASoff_raw_cov.mat",{"covar":cov_raw.data})
 
+################
+### uncomment to use matlab matrix to create new raw with our processed data
+# datarec_fos = mat73.loadmat('1000nam_default_IASoff_fos.mat')
+# data = datarec_fos["data_rec_fos"]
+# ##sss
+# # datarec_vsh = mat73.loadmat('1000nam_default_IASoff_sss.mat')
+# # data = datarec_vsh["data_rec_vsh"]
+# ##test one processed set at a time
+# raw = mne.io.RawArray(data, correct_raw.info)
+# del data #for memory'
 ##################
-# Load processed fif file 
-# raw_dir = '~/RESEARCH/Data/fosters_processed'
-# raw_file = 'phantom_32_200nam_20240708_fost_raw.fif'
-# # Load the continuous raw data #
-# raw = mne.io.read_raw_fif(os.path.join(raw_dir, raw_file),
-#                           preload=False, allow_maxshield='yes')
-# with raw.info._unlock():
-#     raw.info.update(raw_chpi.info)
-# del raw_chpi
-###################
-
-# Plot spectrum, narrowed down to the 4 Hz dipole repetition rate #
-# The 4 Hz component and its expected harmonics are quite apparent, esp. in frontal left quadrant.
-# spect = raw.compute_psd()
-# spect.plot(fmin=0, fmax=40, n_fft=2**13, show=False);
-# spect.plot(average=True, picks="meg", exclude="bads", amplitude=False)
 
 # First, the frequencies: where they should be.
 chpi_freqs, ch_idx, chpi_codes = mne.chpi.get_chpi_info(info=raw.info)
 print(f'cHPI coil frequencies extracted from raw: {chpi_freqs} Hz')
-
-# Look at the relevant stored HPI information (goodness of fit, coordinate transform) #
-# Other data can be found at raw.info['hpi_meas'] and ['hpi_results']; mne.channels.DigMontage() is empty
-# raw.info['dig']
 
 
 # ##########################
@@ -67,31 +54,16 @@ print(f'cHPI coil frequencies extracted from raw: {chpi_freqs} Hz')
 # phantom stimulation protocol. Otherwise, need a custom algorithm.
 events = mne.find_events(raw, stim_channel='SYS201', shortest_event=0)
 
-### Plot the events #
-# id_list = np.unique(events[:,2])
-# if len(id_list) > 10:
-#     color_dir = {x:'b' for x in id_list}
-# else:
-#     color_dir = None
-
-# fig = mne.viz.plot_events(events, sfreq=raw.info['sfreq'], event_id=None,
-#                           first_samp=raw.first_samp, show=False, color=color_dir);
-# fig.gca().set_xlim(None, raw.times[-1]);
-# fig.gca().get_legend().remove()
-# fig.show()
-
 # Create the epochs, assuming bursts of 2 20Hz cycles separated by .25 seconds #
 # The baseline substraction should automatically be applied.
 reject_dict = dict(grad=5000e-12, mag=15000e-12)
 epochs = mne.Epochs(raw, events, tmin=-.05, tmax=0.15, baseline=(None, 0),
                     reject = reject_dict, preload=True, verbose=False)
 print(f'{len(epochs)} epochs created from {len(events)} events.')
-del raw_short #to save space
-
+del raw #save space
 ## Display the waveforms as butterfly plots #
 scalings = dict(mag=1e-11, grad=2e-10)
 epochs.plot(butterfly=False, scalings=scalings);
-
 
 ##################
 #### calculate and examine waveform averages
@@ -99,16 +71,9 @@ epochs.plot(butterfly=False, scalings=scalings);
 # This creates a list of evoked objects, one for each phantom dipole location #
 evokeds = epochs.average(picks='data', by_event_type=True);
 
-### save evoked to put into matlab ####
-evoked = epochs.average()
-evoked.plot(time_unit="s")
-#evoked.save('phantom_32_200nam_20240708_evoked.fif')
-##################
-
-
 ## Plot the evoked waveforms (one dipole at a time)  butterfly style #
 ## Large SNR for all dipoles
-idx = 1
+idx = 20
 fig, axset = plt.subplots(2, 1, figsize=[8, 6])  # for two axes side-by-side
 evokeds[idx].plot(axes=axset, show=False);
 fig.suptitle(f'Dipole {evokeds[idx].comment}', x=0.96, y=0.98,
@@ -118,8 +83,6 @@ fig.show();
 # Distill each evoked object down to one value for each channel (per dipole) #
 # No need to fit whole waveform. So could use single time point, max peak-to-peak
 # amplitude, RMS energy, bandpass power, etc. 
-
-#### whats this??
 # Method 1 - manually choose a high SNR time point; retains polarity
 tpeak = .0375   # based on above average plots
 ipeak = evokeds[0].time_as_index(.0375)[0]   # a useful, but don't need it here
@@ -128,7 +91,6 @@ evokeds_amp = []
 for evk in evokeds:
     evk_new = evk.copy().crop(tpeak, tpeak)
     evokeds_amp.append(evk_new)
-
 
 # Some setup, specific to the Megin/Elekta phantom  #
 pos_actual, ori_actual = mne.dipole.get_phantom_dipoles()
